1c7a5f6531987733febbb2d5ea1fc7dcf54586ba
jverecken <45732366+jverecken@users.noreply.github.com>
2018-12-14 18:47:55 +0100

    Linear Feature selection + Main cleanup (+plot methods)
    1. PCA and Correlation (supervised) selection
    2. Main arrangement + RFBN optimize
    3. Plot functions

--- validation_methods.py
+++ validation_methods.py
@@ -52,7 +52,7 @@ def kfold(model,LM,*is_leave_one_out):
     if len(is_leave_one_out)==1:
         N=length_dataset
     else:
-        N = 3
+        N = 5
         
     length_block =int(length_dataset/N);
     error_Meta = np.zeros((N,))
